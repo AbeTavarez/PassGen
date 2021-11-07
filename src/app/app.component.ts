f4bcd99c943286664a1e82d6f2631eaf74e7c2f8
abeTavarez <abeincwireless@gmail.com>
2021-11-07 14:49:01 -0500

    html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passgen';
+  onButtonClick() {
+    console.log('button was clicked');
+    
+  }
 }
