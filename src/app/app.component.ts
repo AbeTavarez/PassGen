c0762a50f7758180bae1d4210ad73478fe13ca15
abeTavarez <abeincwireless@gmail.com>
2021-11-07 18:34:03 -0500

    input handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  includeLetters = false;
+  includeLetters: Boolean = false;
+  includeNumbers: Boolean = false;
+  includeSymbols: Boolean = false;
   password: string = '';
 
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
+  };
+
+  onChangeUseNumbers() {
+    return this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    return this.includeSymbols = !this.includeSymbols;
   }
 
   onButtonClick() {
     console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
     
     this.password = 'my password!!';
     
