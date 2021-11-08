fef1647a0886ba6352cc85c3fa92db374c79c3ed
abeTavarez <abeincwireless@gmail.com>
2021-11-07 21:12:25 -0500

    generating password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,11 @@ export class AppComponent {
   includeLetters: Boolean = false;
   includeNumbers: Boolean = false;
   includeSymbols: Boolean = false;
+  length: number = 0;
   password: string = '';
 
   onChangeUseLetters() {
-    this.includeLetters = !this.includeLetters;
+    return this.includeLetters = !this.includeLetters;
   };
 
   onChangeUseNumbers() {
@@ -24,13 +25,44 @@ export class AppComponent {
     return this.includeSymbols = !this.includeSymbols;
   }
 
+  onChangeLength(event: any) {
+    const parseValue = parseInt(event?.target?.value);
+    console.log(parseValue);
+    
+
+    if (!isNaN(parseValue)) {
+      this.length = parseValue;
+    }
+  }
+
   onButtonClick() {
-    console.log(this.includeLetters);
-    console.log(this.includeNumbers);
-    console.log(this.includeSymbols);
     
-    this.password = 'my password!!';
-    
+    const numbers: string = '1234567890';
+    const letters: string = 'abcdefghijkmnopqrstuvwxyz';
+    const symbols: string = '!@#$%^&*()_+';
+
+    let validChars: string = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols
+    }
+
+    let generatedPassword: string = '';
+
+    for (let i=0; i<this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+     generatedPassword += validChars[index];
+      
+    }
+    this.password = generatedPassword;
   }
 
 
